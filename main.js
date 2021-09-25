52ff184411fd81b812d8a99f9d044a09b2522ad8
Dre23er <79549421+Dre23er@users.noreply.github.com>
2021-09-25 16:47:56 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+var oliympic
+
+var oliympic = ["https://upload.wikimedia.org/wikipedia/commons/thumb/5/5c/Olympic_rings_without_rims.svg/1200px-Olympic_rings_without_rims.svg.png"]
+console.log(oliympic).innerHTML;
